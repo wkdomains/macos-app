bc54727742576b7f90336dee9c7c1407785a54a4
aa <oneone@gmail.com>
2026-05-02 15:49:01 -0700

    1 window

--- macos-app/macos_appApp.swift
+++ macos-app/macos_appApp.swift
@@ -9,11 +9,21 @@ import SwiftUI
 
 @main
 struct macos_appApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
     @StateObject private var appModel = AppModel()
 
     var body: some Scene {
-        WindowGroup {
+        Window("wkdomains", id: "main") {
             ContentView(browser: appModel.browser)
         }
+        .commands {
+            CommandGroup(replacing: .newItem) {}
+        }
+    }
+}
+
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        true
     }
 }
