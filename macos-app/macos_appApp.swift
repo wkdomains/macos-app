5163b369aacaffc4b447b81024858595ba198b15
aa <oneone@gmail.com>
2026-05-02 13:05:30 -0700

    first

---
+++ macos-app/macos_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  macos_appApp.swift
+//  macos-app
+//
+//  Created by aa on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct macos_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
